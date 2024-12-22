6b4a6492c4881a3061046da75b0995e5ed284e68
GuilhermeMauricio <Guilhermemauriciolima@gmail.com>
2024-12-22 12:14:37 -0300

    Exercises 1 completed

---
+++ Exercises on 22.12/1 - Introduction to Variables and Operators.py
@@ -0,0 +1,10 @@
+Number1=int(input("enter the first number: "))
+Number2=int(input("enter the second number: "))
+
+print("the sum is",Number1+Number2)
+print("---------------------------------")
+print("the subtraction is",Number1-Number2)
+print("---------------------------------")
+print("the multiplication is",Number1*Number2)
+print("---------------------------------")
+print("the division is",Number1/Number2)
