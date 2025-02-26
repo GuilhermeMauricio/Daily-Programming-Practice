43a9d0ff1f4417fc1dadaaf41c9cf28eb61139f5
GuilhermeMauricio <Guilhermemauriciolima@gmail.com>
2025-02-26 15:41:29 -0300

    finish, he  was resolved with list and sorted

--- Beecrowd/1042 - Simple Sort.py
+++ Beecrowd/1042 - Simple Sort.py
@@ -4,4 +4,11 @@ lista=[]
 lista.append(n1)
 lista.append(n2)
 lista.append(n3)
-
+lista_ordenada=sorted(lista)
+print(lista_ordenada[0])
+print(lista_ordenada[1])
+print(lista_ordenada[2])
+print(' ')
+print(n1)
+print(n2)
+print(n3)
