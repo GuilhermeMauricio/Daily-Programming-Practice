540e013bc2318674ac6fcf3bf2d2588836bfd076
Guilherme Mauricio Lima <2303163@SALAS.AULAS>
2025-02-27 18:38:29 -0300

    complete

--- Beecrowd/1043 - Triangle.py
+++ Beecrowd/1043 - Triangle.py
@@ -1,14 +1,9 @@
 a, b, c = map(float,input().split(' '))
+perimetro=a+b+c
+area=(((a+b)*c)/2)
 
-
-if a+b>c and b+c>a:
-    perimetro=a+b+c
-    print(f'Perimetro = {perimetro:.1f}')
-
-elif a+c>b:
-    perimetro=a+b+c
-    print(f'Perimetro = {perimetro:.1f}')
+if a+b>c and b+c>a and c+a>b:
+    print(f"Perimetro = {perimetro:.1f}")
 
 else:
-    area= ((a+b)*c)/2
-    print(f'Area = {area:.1f}')
+    print(f"Area = {area:.1f}")
